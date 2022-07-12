26ed6b82dcf3b99c90f75d167a00a0e709af8541
Aparnnaraj <aparnnaraj232020@gmail.com>
2022-07-12 10:55:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const form =document.getElementsByTagName('form')[0];
+function validate() {
+    var username = document.getElementById("username")
+    var password = document.getElementById("pwd")
+    var regexp = /^([a])([d])([m])([i])([n])$/
+    var regexp2 =/^([1])([2])([3])([4])([5])$/
+
+
+
+    if(username.value.trim()==="" || password.value.trim()===""){
+        alert("Empty fields");
+        return false;
+}
+else if(!regexp.test(username.value)) {
+    alert("Invalid Username ");
+    return false;
+}
+else if(!regexp2.test(password.value)) {
+    alert("Invalid Password");
+    return false;
+}
+else {
+    return true;
+}
+}
+
+form.addEventListener('submit' , function (event) {
+    if(!validate()) {
+        event.preventDefault();
+    }
+});
+
+
